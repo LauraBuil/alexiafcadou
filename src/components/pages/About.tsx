06f7ab0f1956ce6aacbfc13ca8d1ed05535b98d0
Yhumi0210 <buillaura0210@gmail.com>
2025-02-25 18:01:41 +0100

    about

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,7 +1,19 @@
 export default function About() {
     return (
-        <main className="">
-            <h1 className="">Coucou c'est alexia</h1>
-        </main>
+        <section className="about">
+            <a href="https://www.artstation.com/afcvisual" className="about__link">
+                Mon artstation
+            </a>
+            <a href="https://www.instagram.com/alexia_f.c/" className="about__link">
+                Mon Instagram
+            </a>
+            <a href="https://linktr.ee/alexia.f.cadou?utm_source=linktree_profile_share&ltsid=f900a5a2-9e26-4b6e-9c24-49b1b52d681b" className="about__link">
+                Mon linktree
+            </a>
+            <a href="mailto:exemple@email.com?subject=Bonjour&body=Voici%20un%20message" className="about__link">
+                Envoyez-moi un mail
+            </a>
+            <h3 className="about__text">Bienvenue ! Je suis Alexia, Illustratrice Freelance spécialisée dans le réalisme et le fantastique. Polyvalente, je crée aussi bien des environnements immersifs que des props et personnages uniques. Découvrez mon univers et donnons vie à vos projets ensemble !</h3>
+        </section>
     );
 }
