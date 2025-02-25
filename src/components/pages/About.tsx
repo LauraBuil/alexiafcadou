be6ff38738b292ab05ab803693bb905122db6b5d
Yhumi0210 <buillaura0210@gmail.com>
2025-02-25 18:02:46 +0100

    about

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -10,7 +10,7 @@ export default function About() {
             <a href="https://linktr.ee/alexia.f.cadou?utm_source=linktree_profile_share&ltsid=f900a5a2-9e26-4b6e-9c24-49b1b52d681b" className="about__link">
                 Mon linktree
             </a>
-            <a href="mailto:exemple@email.com?subject=Bonjour&body=Voici%20un%20message" className="about__link">
+            <a href="mailto:alexia.f.cadou@gmail.com?subject=Bonjour&body=Voici%20un%20message" className="about__link">
                 Envoyez-moi un mail
             </a>
             <h3 className="about__text">Bienvenue ! Je suis Alexia, Illustratrice Freelance spécialisée dans le réalisme et le fantastique. Polyvalente, je crée aussi bien des environnements immersifs que des props et personnages uniques. Découvrez mon univers et donnons vie à vos projets ensemble !</h3>
