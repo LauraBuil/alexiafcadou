41d1393a432bf422c64ded5a2a60228df86f3cc3
Yhumi0210 <buillaura0210@gmail.com>
2025-02-25 21:00:37 +0100

    header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,23 @@
-import logo from "../../assets/img/logo.png";
-import { Link } from 'react-router-dom';
+import logo from '../../assets/img/logo.png';
+import {Link, useLocation} from 'react-router-dom';
 
 export default function Header() {
+    const location = useLocation();
+    const isActive = (path: string) => {
+        if (path === '/' && location.pathname === '/') return true;
+        if (path !== '/' && location.pathname.includes(path)) return true;
+        return false;
+    };
+
     return (
         <header className='header'>
             <Link to={'/'} className="header__content">
                 <img src={logo} alt='logo' className='header__content__logo'/>
-                <h1 className="header__content__title">ALEXIA F. <br/><span className="blue header__title__name">CADOU</span></h1>
+                <h1 className="header__content__title">ALEXIA F. <br/><span className="blue header__content__title__name">CADOU</span></h1>
             </Link>
             <nav className="header__nav">
-                <Link className="header__nav__item" to={'/'}>Home</Link>
-                <Link className="header__nav__item" to={'/about'}>About</Link>
+                <Link className={`header__nav__link ${isActive('/') ? 'active' : ''}`} to={'/'}>Home</Link>
+                <Link className={`header__nav__link ${isActive('/about') ? 'active' : ''}`} to={'/about'}>About</Link>
             </nav>
         </header>
     );
